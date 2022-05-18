fc6d7ebc9b6a716749ba71594dbfd4285487a6e5
sheyko <sheyko.wk@gmail.com>
2022-05-17 21:51:28 -0500

    add post todos service

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 //Imports
-const express = require('express');
-const fs = require('fs');
-const path = require('path')
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
 
 // Server configurations
-const app = express()
-const databasePath = path.resolve('database.json')
+const app = express();
+const databasePath = path.resolve("database.json");
 
-const PORT = 8000
+const PORT = 8000;
 
-const databaseRead = fs.readFileSync(databasePath, 'utf-8')
-const database = JSON.parse(databaseRead)
+const databaseRead = fs.readFileSync(databasePath, "utf-8");
+const database = JSON.parse(databaseRead);
+
+app.use(express.json())
+
 // Routes
-app.get('/todos', (req, res) => {
-    res.json(database)
-})
+app.get("/todos", (req, res) => {
+  res.json(database);
+});
 
-app.post('/todos')
-app.delete('/todos/:id')
+app.post("/todos", (req, res) => {
+  const data = [...database, req.body];
+
+  fs.writeFileSync(databasePath, JSON.stringify(data));
+
+  res.status(201).json({message: "TODO creado exitosamente"});
+});
+
+app.delete("/todos/:id");
 
 app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`)
-})
+  console.log(`Server started at port ${PORT}`);
+});
